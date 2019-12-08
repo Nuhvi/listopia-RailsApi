9172dd9851edad85bfdec84508cabcd628b64d9f
nazeh <ar.nazeh@gmail.com>
2019-12-08 13:17:16 +0300

    Refactor :hammer: remove unutilized Endpoints.
    Temporarely remove unutilized API endpoints  from routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # devise_for :users
   namespace  :api do
     namespace  :v1 do
-      resources :postings
+      resources :postings, only: [:index]
       resources :favorites, only: [:index]
       resources :favorites do
         member do
